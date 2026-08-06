d75b4921df61431bc1165c8a2ab4098ae881b476
iyenagbe <iyenagberaymond@gmail.com>
2026-08-06 16:56:42 +0100

    structuring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
 
     <div>
-        <h1>Doctor Appointment System</h1>
+        <h1 className="text-3xl font-bold text-amber-300">Doctor Appointment System</h1>
     </div>
   )
 }
