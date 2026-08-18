b51c0dc08244d64c0b3f1e39bfbf1fa4e2cccabe
iyenagbe <iyenagberaymond@gmail.com>
2026-08-18 18:50:53 +0100

    creating navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo}alt="" />
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-44 cursor-pointer' src={assets.logo}alt="" />
+        <ul className='hidden md:flex items-start gap-4 font-medium'>
+            <NavLink>
+                <li className='py-1'>Home</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            </NavLink>
+
+            <NavLink>
+                <li className='py-1'>ALL Doctors</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            </NavLink>
+
+            <NavLink>
+                <li className='py-1'>About</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            </NavLink>
+
+            <NavLink>
+                <li className='py-1'>Contact</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            </NavLink>
+        </ul>
+
+        <div>
+            <button>create account</button>
+        </div>
     </div>
   )
 }
