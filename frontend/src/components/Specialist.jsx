d8ee39f328dc1b0683fb9123c85394ea97e141ea
iyenagbe <iyenagberaymond@gmail.com>
2026-09-01 20:24:43 +0100

    creating specialist menu

--- frontend/src/components/Specialist.jsx
+++ frontend/src/components/Specialist.jsx
@@ -7,13 +7,15 @@ const Specialist = () => {
 
   return (
 
-    <div id='specialist' className=''>
-        <h1>Find the best specialist</h1>
-        <p>Get access to a network of top-rated specialists in your area.</p>
-        <div>
-            {specialistData.map((item, ) => (
-              <Link>
+    <div id='specialist' className='flex flex-col items-center py-16 gap-4 text-gray-700'>
+        <h1 className='text-3xl font-bold'>Find the best specialist</h1>
+        <p className='text-center sm:w-1/3 text-sm'>Get access to a network of top-rated specialists in your area.</p>
+        <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
+            {specialistData.map((item, index) => (
 
+              <Link className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.Specialist}`} >
+                <img className='w-16 sm:w-24 mb-2' src={item.image} alt='' />
+                <p>{item.Specialist}</p>
               </Link>
             ))}
         </div>
