8ac8110d7fa74d06b4a970a35420c5e882e1cfec
iyenagbe <iyenagberaymond@gmail.com>
2026-09-01 19:14:23 +0100

    creating hero section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,18 +4,18 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex flex-col md:flex-row flex-wrap bg-blue-500 rounded-lg px-6 md:px-10 lg:px-20'>
         {/*---- left section-----*/}
-        <div>
-            <p>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb[-30px]'>
+            <p className='text-3xl md:text-3xl lg:text-3xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
                 Book an appointment with the <br /> best doctors in your city.
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-27' src={assets.group_profiles} alt="" />
                 <p>Simplify your healthcare experience</p>
             </div>
-            <a href=''>
-                Book an appointment <img src={assets.arrow_icon} alt="" />
+            <a href='#speciality' className='flex items-center gap-2 bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+                Book an appointment <img className='w-3' src={assets.arrow_icon} alt="" />
 
             </a>
         </div>
@@ -23,8 +23,8 @@ const Header = () => {
 
 
         {/*---- right section-----*/}
-        <div>
-            <img src={assets.header_img} alt="" />
+        <div className='md:w-1/2 relative'>
+            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
 
         </div>
     </div>
