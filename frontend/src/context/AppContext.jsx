8bf52ac77b18da9bd2f7d251e49cc02c7461fde9
iyenagbe <iyenagberaymond@gmail.com>
2026-09-11 11:10:58 +0100

    fix a bug

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { doctors } from "../assets/assets";
 
 export const AppContext = createContext()
 
-const AppContextProvider = () => {
+const AppContextProvider = (props) => {
 
     const value = {
         doctors
@@ -14,9 +14,6 @@ const AppContextProvider = () => {
             {props.children}
        </AppContext.Provider>
     )
-
-
-
 
 }
 
