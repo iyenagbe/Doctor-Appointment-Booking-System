b0c95a065aca3ec0997d31e1468c93560bae7748
iyenagbe <iyenagberaymond@gmail.com>
2026-09-16 07:22:31 +0100

    create project barner

--- frontend/src/components/Barner.jsx
+++ frontend/src/components/Barner.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Barner = () => {
   return (
-    <div>Barner</div>
+    <div>
+        {/* Barner Lift Side Content */}
+        <div>
+            <div>
+                <p>Book Appointment</p>
+                <p>With Over 100+ Trusted Doctors</p>
+            </div>
+            <button>Create Account</button>
+        </div>
+
+        {/* Barner Right Side Content */}
+        <div>
+            <img src={assets.appointment_img} alt="" />
+        </div>
+
+
+    </div>
+
+
   )
 }
 
