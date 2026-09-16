3df5a2d22a84bf957f8534a13a532cc80d3c391d
iyenagbe <iyenagberaymond@gmail.com>
2026-09-16 11:08:51 +0100

    creating  doctors appointment section

--- frontend/src/components/Barner.jsx
+++ frontend/src/components/Barner.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Barner = () => {
+
+    const navigate = useNavigate()
+
+
+
   return (
-    <div>
+    <div className='flex bg-blue-500 rounded-lg px-5 sm:px-14 lg:px-12 my-20 md:mx-10'>
         {/* Barner Lift Side Content */}
-        <div>
-            <div>
-                <p>Book Appointment</p>
-                <p>With Over 100+ Trusted Doctors</p>
+        <div className='flex-1 py-7 sm:py-10 md:py-16 lg:py-24 lg:pl-5'>
+            <div className='text-x1 sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-white'>
+                <p className=''>Book Appointment</p>
+                <p className='mt-4'>With Over 100+ Trusted Doctors</p>
             </div>
-            <button>Create Account</button>
+            <button onClick={() => {navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Account</button>
         </div>
 
         {/* Barner Right Side Content */}
-        <div>
-            <img src={assets.appointment_img} alt="" />
+        <div className='hidden md:block md:w-1/3 lg:w-\[379px\] relative '>
+            <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
         </div>
 
 
--- frontend/src/components/RankingDoctor.jsx
+++ frontend/src/components/RankingDoctor.jsx
@@ -17,7 +17,7 @@ const RankingDoctor = () => {
         <div className='w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0,10).map((item, index) => (
            <div onClick={() => navigate(`/appointment/${item._id}`)} key={index} className='border border-blue-300 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500'>
-            <img className='bg-blue-50' src={item.image} alt="" />
+            <img className='bg-blue-500' src={item.image} alt="" />
             <div className='p-4'>
                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                     <p className='w-2 h-2 bg-green-500 rounded-full'></p>
